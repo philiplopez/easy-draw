46f2312c77070b4d1c1f16fc500b3f3a0374526b
Philip Lopez <philip.lopez@philiplopez.com>
2017-04-05 10:00:30 +1000

    Introduced style in types

--- src/namedAPI.ts
+++ src/namedAPI.ts
@@ -2,20 +2,26 @@ import * as t from "./types"
 import {Vector2D} from "easy-vector2d"
 export * from "./types"
 
-export function circle(centre_px : Vector2D, radius_px : number) : t.Circle {
-    return {
-        type: "shape",
-        shapeType: "circle",
-        centre_px,
-        radius_px
-    };
+const DEFAULT_STYLE : t.Style = {
+    fillColour: rgba(0, 0, 0, 1),
+    strokeColour: rgba(0, 0, 0, 1)
 }
 
-export function rectangle(topLeft_px : Vector2D, dimensions_px : Vector2D) : t.Rectangle {
-    return {
-        type: "shape",
-        shapeType: "rectangle",
-        topLeft_px,
-        dimensions_px
-    }
+/**
+ *
+ * @param red: 0 to 255 (0 is no red, 255 is full red)
+ * @param green: 0 to 255 (0 is no green, 255 is full green)
+ * @param blue: 0 to 255 (0 is no blue, 255 is full blue)
+ * @param transparency "alpha": 0.0 to 1.0 (0 is transparent, 1.0 is opaque, 0.5 is half-opaque)
+ */
+export function rgba(red : number, green : number, blue : number, alpha : number) : t.RGBA {
+    return {red, green, blue, alpha}
+}
+
+export function circle(centre_px : Vector2D, radius_px : number, style = DEFAULT_STYLE) : t.Circle {
+    return {type: "shape", shapeType: "circle", centre_px, radius_px, style};
+}
+
+export function rectangle(topLeft_px : Vector2D, dimensions_px : Vector2D, style = DEFAULT_STYLE) : t.Rectangle {
+    return {type: "shape", shapeType: "rectangle", topLeft_px, dimensions_px, style}
 }
--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,20 @@
 import {Vector2D} from "easy-vector2d"
 
+export interface RGBA {
+    red: number,
+    green: number,
+    blue: number,
+    alpha: number
+}
+
+export interface Style {
+    fillColour: RGBA,
+    strokeColour: RGBA
+}
+
 export interface Drawable {
-    type: "shape"
+    type: "shape",
+    style: Style
 }
 
 export interface Shape extends Drawable {
